ed7a43afc520fb6d56f94dccf2ce5ddcd0d479fa
nicholas hung <nhung93@outlook.com>
2020-08-19 21:55:46 -0400

    trying pupil tracking

--- basic_test.py
+++ basic_test.py
@@ -1,35 +1,3 @@
-"""
-	The facial landmark code here is pretty much copied (although I hand-typed)
-	from Adrian Rosebrock's amazing tutorial here:
-	https://www.pyimagesearch.com/2017/04/17/real-time-facial-landmark-detection-opencv-python-dlib/
-
-	For the landmark predictor, I used shape_predictor_68_face_landmarks.dat that you can download here:
-	https://github.com/AKSHAYUBHAT/TensorFace/blob/master/openface/models/dlib/shape_predictor_68_face_landmarks.dat
-
-	to get dlib,
-	I did:
-	pip install cmake
-	pip install dlib 
-	
-	I'm using conda as well on Windows 10.
-
-
-	idea:
-	have a local server that gets those facial landmark coords (flask/socketio)
-	send them off to a client browser
-	use those coords to draw the face on a canvas 
-	
-	https://flask-socketio.readthedocs.io/en/latest/
-	
-	then next step:
-	apply to a virtual avatar
-	profit?
-
-	then next next step:
-	how about hand detection?
-
-"""
-
 from imutils.video import VideoStream
 from imutils import face_utils
 from flask import Flask, render_template
@@ -90,7 +58,7 @@ class VStream(threading.Thread):
 				
 				# collect all the landmark coordinates
 				# should be a list of dictionaries like [{'x': a, 'y': b},...]
-				coords = []
+				coords = {'landmark_coords': [], 'pupil_coords': []}
 				
 				# get the facial landmarks of the faces
 				for face in faces:
@@ -112,6 +80,7 @@ class VStream(threading.Thread):
 					# and keep going until you either hit a lighter area again (mark as right bound) or hit the right end of the box
 					# take the middle of the left and right to estimate where the pupil is
 					# https://answers.opencv.org/question/173862/how-to-check-in-python-if-a-pixel-in-an-image-is-of-a-specific-color-other-than-black-and-white-for-grey-image/
+					# https://stackoverflow.com/questions/51781843/color-intensity-of-pixelx-y-on-image-opencv-python
 					
 					shape = self.predictor(gray, face)
 					shape = face_utils.shape_to_np(shape)
@@ -120,17 +89,63 @@ class VStream(threading.Thread):
 					# we need a left coord and a width for left and right eye 
 					# can we index shape?
 					# https://www.pyimagesearch.com/2017/04/10/detect-eyes-nose-lips-jaw-dlib-opencv-python/
-					left_eye_left = shape[36]
-					left_eye_right = shape[40]
+					left_eye_l = (int(shape[36][0]), int(shape[36][1]))
+					left_eye_r = (int(shape[39][0]), int(shape[39][1]))
+					left_slope = (left_eye_r[1] - left_eye_l[1]) / (left_eye_r[0] - left_eye_l[0])
+					left_b = left_eye_l[1] - (left_slope * left_eye_l[0]) # y-intercept from y=mx+b
 					
-					right_eye_left = shape[42] 
-					right_eye_right = shape[45]
+					right_eye_l = (int(shape[42][0]), int(shape[42][1]))
+					right_eye_r = (int(shape[45][0]), int(shape[45][1]))
+					right_slope = (right_eye_r[1] - right_eye_l[1]) / (right_eye_r[0] - right_eye_l[0])
+					right_b = right_eye_l[1] - (right_slope * right_eye_l[0])
 					
 					# estimate pupil location (given by 1 coord)
 					# as as a separate field in the json output? one field for landmarks, one field for pupils
+					left_start = left_eye_l[0] + 1
+					left_end = left_eye_r[0] - 1
+					max = 0 # we're looking for the peak of intensity given 
+					max_coord = []
+					print("=========== left eye ==============")
+					for i in range(left_start, left_end+1):
+						y = (left_slope * i) + left_b
+						intensity = gray[i, int(y)]
+						
+						if max == 0:
+							max = intensity
+							max_coord = [i, int(y)]
+						else:
+							if intensity > max:
+								max = intensity
+								max_coord = [i, int(y)]
+						
+						print(f"intensity @ {i}, {y}: {intensity}")
+						#coords['pupil_coords'].append({'x': i, 'y': y})
+					print("======================")
+					max = 0
+					coords['pupil_coords'].append({'x': max_coord[0], 'y': max_coord[1]})
+					
+					right_start = right_eye_l[0] + 1
+					right_end = right_eye_r[0] - 1
+					print("========= right eye ==============")
+					for i in range(right_start, right_end+1):
+						y = (right_slope * i) + right_b
+						intensity = gray[i, int(y)]
+						
+						if max == 0:
+							max = intensity
+							max_coord = [i, int(y)]
+						else:
+							if intensity > max:
+								max = intensity
+								max_coord = [i, int(y)]
+						
+						print(f"intensity @ {i}, {y}: {intensity}")	
+						#coords['pupil_coords'].append({'x': i, 'y': y})
+					print("======================")
+					coords['pupil_coords'].append({'x': max_coord[0], 'y': max_coord[1]})
 					
 					for (x,y) in shape:
-						coords.append({'x': int(x), 'y': int(y)})
+						coords['landmark_coords'].append({'x': int(x), 'y': int(y)})
 						cv2.circle(frame, (x, y), 1, (255, 0, 0), -1) # BGR format
 						
 				#cv2.imshow("Frame", frame)
@@ -149,7 +164,6 @@ class VStream(threading.Thread):
 		self.video_stream.stop()
 
 @app.route('/')
-@app.route('/vstream_display')
 def vstream_display():
 
 	global BACKGROUND_THREAD # global bg thread variable 
@@ -173,7 +187,7 @@ def vstream_display():
 		BACKGROUND_THREAD.start()
 		logging.info("started background thread...")
 
-	return render_template("vstream_display.html")
+	return render_template("landmark_testing.html")
 
 
 # allow the client to pause/continue the stream 
